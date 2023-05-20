046c0edd16a6bd37abcfe9546030ad2b888ff47c
afonne-cid <delightinbusiness@gmail.com>
2023-05-20 13:17:47 +0100

    Handle Application Errors
    Add error handlers for common exceptions, improving the app's resilience and user experience

---
+++ backend/app/error_handlers.py
@@ -0,0 +1,64 @@
+'''
+This module defines the error handlers.
+'''
+from flask import jsonify
+
+
+def handle_bad_request(error):
+    '''Handle 400 errors
+
+    Args:
+       error (Exception): The ID of the meeting to fetch.
+
+    Returns:
+        The error message supplied.
+    '''
+    return jsonify(error=str(error)), 400
+
+
+def handle_unauthorized(error):
+    '''Handle 401 errors
+
+    Args:
+       error (Exception): The ID of the meeting to fetch.
+
+    Returns:
+        The error message supplied.
+    '''
+    return jsonify(error=str(error)), 401
+
+
+def handle_forbidden(error):
+    '''Handle 403 errors
+
+    Args:
+       error (Exception): The ID of the meeting to fetch.
+
+    Returns:
+        The error message supplied.
+    '''
+    return jsonify(error=str(error)), 403
+
+
+def handle_page_not_found(error):
+    '''Handle 404 errors
+
+    Args:
+       error (Exception): The ID of the meeting to fetch.
+
+    Returns:
+        The error message supplied.
+    '''
+    return jsonify(error=str(error)), 404
+
+
+def handle_internal_server_error(error):
+    '''Handle 500 errors
+
+    Args:
+       error (Exception): The ID of the meeting to fetch.
+
+    Returns:
+        The error message supplied.
+    '''
+    return jsonify(error=str(error)), 500
