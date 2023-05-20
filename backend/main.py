0bef99a233f1f42649322cfe35f629fc6ca6979a
afonne-cid <delightinbusiness@gmail.com>
2023-05-20 13:13:30 +0100

    Launch Application
    This commit adds the main entry point for the application, using Flask to run our app.

---
+++ backend/main.py
@@ -0,0 +1,22 @@
+'''
+This module is the entry point of the Flask application. 
+
+It creates an instance of the application and runs it. 
+By default, the application will run in debug mode when run directly.
+When deployed in a production environment, the WSGI server will use the `app` object.
+
+Usage:
+    python3 main.py
+'''
+
+from app import create_app
+
+app = create_app()
+# Flask application instance.
+# A call to create_app creates application instance from the app module.
+
+if __name__ == '__main__':
+    # The application instance will be run directly if this script is executed as the main script.
+    # If this script is imported from another script, the application instance will not run.
+    # Instead, the other script or the WSGI server can use the 'app' object.
+    app.run(debug=True)
