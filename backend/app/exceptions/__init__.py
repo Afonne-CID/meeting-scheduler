9dda365c9cc783d750eb755a50061ecc2ef1b211
afonne-cid <delightinbusiness@gmail.com>
2023-05-22 19:45:09 +0100

    Add more exception handlers

--- backend/app/exceptions/__init__.py
+++ backend/app/exceptions/__init__.py
@@ -1,22 +1,47 @@
-class ResourceCreationError(Exception):
-    """Exception raised for errors in the creation of a resource."""
-
-    def __init__(self, message="Resource creation failed"):
+class ResourceFetchError(Exception):
+    '''Exception raised for errors in fetching a resource.
+    '''
+    def __init__(self, message='Resource fetching failed'):
         self.message = message
         super().__init__(self.message)
 
+class ResourceCreationError(Exception):
+    '''Exception raised for errors in the creation of a resource.
+    '''
+    def __init__(self, message='Resource creation failed'):
+        self.message = message
+        super().__init__(self.message)
+
+class ResourceDeletionError(Exception):
+    '''Exception raised for errors in the deletion of a resource.
+    '''
+    def __init__(self, message='Resource deletion failed'):
+        self.message = message
+        super().__init__(self.message)
+
+class ResourceUpdateError(Exception):
+    '''Exception raised for errors while updating a resource.
+    '''
+    def __init__(self, message='Resource update failed'):
+        self.message = message
+        super().__init__(self.message)
 
 class UnauthorizedError(Exception):
-    """Exception raised for unauthorized actions."""
-
-    def __init__(self, message="Unauthorized action"):
+    '''Exception raised for unauthorized actions.
+    '''
+    def __init__(self, message='Unauthorized action'):
         self.message = message
         super().__init__(self.message)
 
 
 class UnexpectedError(Exception):
-    """Exception raised for an unexpected error."""
+    '''Exception raised for an unexpected error.'''
 
-    def __init__(self, message="Unexpected error"):
+    def __init__(self, error=None, message='Unexpected error'):
+        self.error = error
         self.message = message
         super().__init__(self.message)
+
+    def __str__(self):
+        return f'{self.message}: {self.error}'
+
