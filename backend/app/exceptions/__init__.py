0b2933f1a6438b43cc6e0b0e094db66fdfc76e44
Afonne-CID <delightinbusiness@gmail.com>
2023-12-03 06:09:27 +0100

    allow for error object

--- backend/app/exceptions/__init__.py
+++ backend/app/exceptions/__init__.py
@@ -1,37 +1,57 @@
 class ResourceFetchError(Exception):
     '''Exception raised for errors in fetching a resource.
     '''
-    def __init__(self, message='Resource fetching failed'):
+    def __init__(self, error=None, message='Resource fetching failed'):
+        self.error = error
         self.message = message
         super().__init__(self.message)
+
+    def __str__(self):
+        return f'{self.message}: {self.error}'
 
 class ResourceCreationError(Exception):
     '''Exception raised for errors in the creation of a resource.
     '''
-    def __init__(self, message='Resource creation failed'):
+    def __init__(self, error=None, message='Resource creation failed'):
+        self.error = error
         self.message = message
         super().__init__(self.message)
+
+    def __str__(self):
+        return f'{self.message}: {self.error}'
 
 class ResourceDeletionError(Exception):
     '''Exception raised for errors in the deletion of a resource.
     '''
-    def __init__(self, message='Resource deletion failed'):
+    def __init__(self, error=None, message='Resource deletion failed'):
+        self.error = error
         self.message = message
         super().__init__(self.message)
+
+    def __str__(self):
+        return f'{self.message}: {self.error}'
 
 class ResourceUpdateError(Exception):
     '''Exception raised for errors while updating a resource.
     '''
-    def __init__(self, message='Resource update failed'):
+    def __init__(self, error=None, message='Resource update failed'):
+        self.error = error
         self.message = message
         super().__init__(self.message)
+
+    def __str__(self):
+        return f'{self.message}: {self.error}'
 
 class UnauthorizedError(Exception):
     '''Exception raised for unauthorized actions.
     '''
-    def __init__(self, message='Unauthorized action'):
+    def __init__(self, error=None, message='Unauthorized action'):
+        self.error = error
         self.message = message
         super().__init__(self.message)
+
+    def __str__(self):
+        return f'{self.message}: {self.error}'
 
 
 class UnexpectedError(Exception):
